ffcd1fb9dbccf6b7f7f6b0e343c716461a33f500
eljoaquin <eljoaquin27@gmail.com>
2015-01-22 22:33:13 +0000

    wrap all js code with jquery ready

--- js/main.js
+++ js/main.js
@@ -1,80 +1,73 @@
- $(function() {
-
-  $(".im").draggable();
-
-
-
-
-  $(".placeholder").droppable({
-   drop: function(event, ui) {
-
-
-    var top = $(this).offset().top;
-    var left = $(this).offset().left;
-
-
-    if ($(this).children().length > 0) {
-     $(ui.draggable[0]).css('top', (parseInt($(ui.draggable[0]).css('top'), 10)-170)+'px');
-     return false;
-    }
-
-
-    $(ui.draggable[0]).attr('style', '');
-    $(ui.draggable[0]).appendTo($(this))
-    $(ui.draggable[0]).one('mousedown', function() {
-     $(this).css('top', top);
-     $(this).css('left', left);
-     $(this).appendTo($('.temp'))
-    })
-    $(ui.draggable[0]).one('mouseup', function() {
-     $(this).draggable();
-    })
-
-   }
-  });
-  
- $('#go').on('click', function() {
-   $( ".placeground" ).fadeOut(500, function() {
-    // Animation complete.
+$(document).ready(function() {
+    $(".im").draggable();
     
-    $('.placeholder').each(function() {
-     if($(this).children().length > 0) {
-      var html = $(this).html();
-      $('.gallery').append(html);
-      $('.gallery img').hide();
-     }
+    $(".placeholder").droppable({
+        drop: function(event, ui) {
+            var top = $(this).offset().top;
+            var left = $(this).offset().left;
+
+            if ($(this).children().length > 0) {
+                $(ui.draggable[0]).css('top', (parseInt($(ui.draggable[0]).css('top'), 10) - 170) + 'px');
+                return false;
+            }
+
+
+            $(ui.draggable[0]).attr('style', '');
+            $(ui.draggable[0]).appendTo($(this));
+            $(ui.draggable[0]).one('mousedown', function() {
+                $(this).css('top', top);
+                $(this).css('left', left);
+                $(this).appendTo($('.temp'));
+            });
+            $(ui.draggable[0]).one('mouseup', function() {
+                $(this).draggable();
+            });
+
+        }
     });
     
-    $('.gallery img').first().addClass('next');
-    
-    $('.gallery').show();
-    
-    $('#go').hide();
-    $('#back').show();
-    
-  });
- })
- $('#back').on('click', function() {
-  $('.gallery').html('');
-  $('.gallery').hide();
-  $( ".placeground" ).fadeIn(500);
-  $('#back').hide();
-    $('#go').show();
- });
- 
- setInterval(function(){ 
-  $('.gallery').find('.next').fadeIn(1000, function() {
-   $(this).fadeOut(1000, function() {
-     console.log($('.gallery').find('.next').next());
-     if($('.gallery').find('.next').next().length > 0) {
-      $('.gallery').find('.next').removeClass('next').next().addClass('next'); 
-     } else {
-      $('.gallery').find('.next').removeClass('next');
-      $('.gallery img').first().addClass('next');
-     }
-       
-   })
-  });
-  
- }, 3000);
- });
+    $('#go').on('click', function() {
+        $(".placeground").fadeOut(500, function() {
+            // Animation complete.
+            $('.placeholder').each(function() {
+                if ($(this).children().length > 0) {
+                    var html = $(this).html();
+                    $('.gallery').append(html);
+                    $('.gallery img').hide();
+                }
+            });
+
+            $('.gallery img').first().addClass('next');
+
+            $('.gallery').show();
+
+            $('#go').hide();
+            $('#back').show();
+
+        });
+   });
+   
+   $('#back').on('click', function() {
+       $('.gallery').html('');
+       $('.gallery').hide();
+       $(".placeground").fadeIn(500);
+       $('#back').hide();
+       $('#go').show();
+   });
+   
+   
+   setInterval(function() {
+       $('.gallery').find('.next').fadeIn(1000, function() {
+           $(this).fadeOut(1000, function() {
+        
+               if ($('.gallery').find('.next').next().length > 0) {
+                   $('.gallery').find('.next').removeClass('next').next().addClass('next');
+               } else {
+                   $('.gallery').find('.next').removeClass('next');
+                   $('.gallery img').first().addClass('next');
+               }
+
+           });
+       });
+    }, 3000);
+});
